aa6a1c13217f8a2deb1bfb055a10aac29e9844b2
EddieKodeHode <eddie.kodehode@gmail.com>
2021-10-08 14:19:44 +0200

    IT WORKS

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ let highScore = document.getElementById("high-score")
 
 let count = 0
 
+let countIncrease = 1
+
 function increment() {
-    count = count + 1
+    count += countIncrease
     highScore.innerText = count
 }
 
-// let upgradeButton = document.getElementById("upgrade")
+let upgradeButton = document.getElementById("upgrade")
 
-// function upgrade() {
-//     if count >= 100 {
-//         count = count - 100
-//         highScore.innerText = count
-//     }
-//     else {
-//     }
-// }
+function upgrade() {
+    if (count >= 100) {
+        count = count - 100
+        highScore.innerText = count
+        countIncrease += 1
+    }
+    else { 
+    }
+}

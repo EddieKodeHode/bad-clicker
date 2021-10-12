5f1cac7f55b9548405f9a837c8ca037f13f38899
EddieKodeHode <eddie.kodehode@gmail.com>
2021-10-12 12:40:22 +0200

    Added functional duplicator item upgrades. Needs balanced

--- index.js
+++ index.js
@@ -25,6 +25,200 @@ let level = 1
 let duplicator = 1.0
 // Current duplicator level. starts at 1.0.
 
+let dupeNumber = 0.5
+// Current duplicator increase number.
+
+function duplicate() {
+    duplicator += dupeNumber
+    duplicateValue.innerText = duplicator
+}
+// Duplication function. Change dupeNumber to increase/decrease dupe value.
+// Call this function inside another function.
+
+let kibbleImg = document.getElementById("kibble-img")
+let toyImg = document.getElementById("toy-img")
+let friendImg = document.getElementById("friend-img")
+// Set destination for upgrade purchase graphics
+
+let buttonKibble = document.getElementById("button-kibble")
+let buttonToy = document.getElementById("button-toy")
+let buttonFriend = document.getElementById("button-friend")
+// 
+
+let $kibble0 = ""
+let $kibble1 = "url(images/kibble1.png)"
+let $kibble2 = "url(images/kibble2.png)"
+let $kibble3 = "url(images/kibble3.png)"
+
+let $toy0 = ""
+let $toy1 = "url(images/toy1.png)"
+let $toy2 = "url(images/toy2.png)"
+let $toy3 = "url(images/toy3.png)"
+
+
+let $friend0 = ""
+let $friend1 = "url(images/friend1.png)"
+let $friend2 = "url(images/friend2.png)"
+let $friend3 = "url(images/friend3.png)"
+// Set image source for upgrade graphics
+
+let kibbleLevel = document.getElementById("kibble-level")
+let toyLevel = document.getElementById("toy-level")
+let friendLevel = document.getElementById("friend-level")
+
+let kibbleCost = document.getElementById("kibble-cost")
+let toyCost = document.getElementById("toy-cost")
+let friendCost = document.getElementById("friend-cost")
+
+let kibbleC = 1000
+let toyC = 100000
+let friendC = 1000000
+
+let kibbleL = 0
+let toyL = 0
+let friendL = 0
+
+function updateKibbleButton() {
+    if (count >= kibbleC) {
+        buttonKibble.style.color="var(--secondary-color-light)"
+        buttonKibble.style.border="3px outset var(--secondary-color)"
+        buttonKibble.style.backgroundColor="var(--secondary-color)"
+    }
+    else {
+        buttonKibble.style.color="var(--dark-gray)"
+        buttonKibble.style.border="3px outset var(--dark-gray)"
+        buttonKibble.style.backgroundColor="var(--light-gray)"
+    }
+}
+
+function updateToyButton() {
+    if (count >= toyC) {
+        buttonToy.style.color="var(--secondary-color-light)"
+        buttonToy.style.border="3px outset var(--secondary-color)"
+        buttonToy.style.backgroundColor="var(--secondary-color)"
+    }
+    else {
+        buttonToy.style.color="var(--dark-gray)"
+        buttonToy.style.border="3px outset var(--dark-gray)"
+        buttonToy.style.backgroundColor="var(--light-gray)"
+    }
+}
+
+function updateFriendButton() {
+    if (count >= friendC) {
+        buttonFriend.style.color="var(--secondary-color-light)"
+        buttonFriend.style.border="3px outset var(--secondary-color)"
+        buttonFriend.style.backgroundColor="var(--secondary-color)"
+    }
+    else {
+        buttonFriend.style.color="var(--dark-gray)"
+        buttonFriend.style.border="3px outset var(--dark-gray)"
+        buttonFriend.style.backgroundColor="var(--light-gray)"
+    }
+}
+
+function kibbleUp() {
+    if ( count >= kibbleC ) {
+        count = count - kibbleC
+        kibbleL += 1
+        kibbleLevel.innerText = kibbleL
+        duplicate()
+        kibbleC = ( kibbleC * 2)
+        kibbleCost.innerText = kibbleC
+        upgradeAll()
+    }
+}
+
+function toyUp() {
+    if ( count >= toyC ) {
+        count = count - toyC
+        toyL += 1
+        toyLevel.innerText = toyL
+        duplicate()
+        toyC = ( toyC * 2)
+        toyCost.innerText = toyC
+        upgradeAll()
+    }
+}
+
+function friendUp() {
+    if ( count >= friendC ) {
+        count = count - friendC
+        friendL += 1
+        friendLevel.innerText = friendL
+        duplicate()
+        friendC = ( friendC * 2)
+        friendCost.innerText = friendC
+        upgradeAll()
+    }
+}
+
+
+function kibbleImage() {
+    if (kibbleL >= 20) {
+        kibbleImg.style.backgroundImage=$kibble3
+    }
+    else if (kibbleL >= 10) {
+        kibbleImg.style.backgroundImage=$kibble2
+    }
+    else if (kibbleL >= 1) {
+        kibbleImg.style.backgroundImage=$kibble1
+    }
+    else {
+        kibbleImg.style.backgroundImage=$kibble0
+    }
+}
+
+function toyImage() {
+    if (toyL >= 20) {
+        toyImg.style.backgroundImage=$toy3
+    }
+    else if (toyL >= 10) {
+        toyImg.style.backgroundImage=$toy2
+    }
+    else if (toyL >= 1) {
+        toyImg.style.backgroundImage=$toy1
+    }
+    else {
+        toyImg.style.backgroundImage=$toy0
+    }
+}
+
+function friendImage() {
+    if (friendL >= 20) {
+        friendImg.style.backgroundImage=$friend3
+    }
+    else if (friendL >= 10) {
+        friendImg.style.backgroundImage=$friend2
+    }
+    else if (friendL >= 1) {
+        friendImg.style.backgroundImage=$friend1
+    }
+    else {
+        friendImg.style.backgroundImage=$friend0
+    }
+}
+
+
+
+
+
+function updateScore() {
+    highScore.innerText = count
+}
+
+function upgradeAll() {
+    catImageUpgrade()
+    updateButton()
+    updateKibbleButton()
+    updateToyButton()
+    updateFriendButton()
+    updateScore()
+    kibbleImage()
+    toyImage()
+    friendImage()
+}
+
 
 function updateButton() {
     if (count >= levelUpCost) {
@@ -43,8 +237,7 @@ function updateButton() {
 function increment() {
     count += (countIncrease * duplicator)
     count = Math.round(count)
-    highScore.innerText = count
-    updateButton()
+    upgradeAll()
 }
 // Function that makes buttonpress increase your count
 // It also checks if the "Level up" button needs to be updated.
@@ -92,44 +285,18 @@ let levelUpCost = 100
 function upgrade() {
     if (count >= levelUpCost) {
         count = count - levelUpCost
-        highScore.innerText = count
         countIncrease += 1
         level += 1
         currentLevel.innerText = level
         levelUpCost = levelUpCost * 1.05
         levelUpCost = Math.round(levelUpCost) //Rounds the number so there's no decimals.
         levelCost.innerText = levelUpCost
-        catImageUpgrade()
-        updateButton()
+        upgradeAll()
     }
     else { 
     }
 }
 // Basic level up.
-
-let dupeNumber = 0.5
-
-function duplicate() {
-    duplicator += dupeNumber
-    duplicateValue.innerText = duplicator
-}
-// Duplication function. Change dupeNumber to increase/decrease dupe value.
-// Call this function inside another function.
-
-let kibbleImg = document.getElementById("kibble-img")
-let toyImg = document.getElementById("toy-img")
-let friendImg = document.getElementById("friend-img")
-// Set destination for upgrade purchase graphics
-
-let $kibble0 = ""
-let $kibble1 = "url(images/kibble1)"
-
-let $toy0 = ""
-let $toy1 = "url(images/toy1.png)"
-
-let $friend0 = ""
-let $friend1 = "url(images/friend1.png)"
-// Set image source for upgrade graphics
 
 
 //  ------------------- TO-DO LIST ----------------------
